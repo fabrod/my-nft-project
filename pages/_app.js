545a0862e532a76d4feb895a15b990cfb52d3f73
fabrod <fabrod035@gmail.com>
2022-10-13 15:38:20 -0400

    initial push

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
